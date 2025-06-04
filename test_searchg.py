e70f602b76b36e986c82e3d866267b300dcdb0b2
Natalya Sviryukova <vertokryl@gmail.com>
2025-06-04 17:20:14 +0300

    Добавила проверку поиска

--- test_searchg.py
+++ test_searchg.py
@@ -2,4 +2,5 @@ from selene import browser, have
 
 def test_search(open_browser_with_size):
     browser.element('[name="q"]').type('qa.guru').press_enter()
+    browser.element('html').should(have.text('https://qa.guru'))
 
